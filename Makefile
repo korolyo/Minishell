4933a5f020bb2aeec7163fdd195a46f060815dfc
korolyo <korolyo@gmail.com>
2022-01-21 20:24:05 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,46 @@
+
+NAME	=	minishell
+
+SOURCES_LIST	= 	main.c\
+
+
+CC		= gcc
+CFLAGS	=	-Werror -Wall -Wextra -g
+LIBRARIES =	-lft\
+	-L$(LIBFT_DIRECTORY) \
+
+INCLUDES = -I$(LIBFT_HEADERS) -I$(HEADERS_DIR)
+
+HEADERS_LIST = \
+	minishell.h
+HEADERS_DIR	=	./includes/
+HEADERS =	$(addprefix $(HEADERS_DIR), $(HEADERS_LIST))
+
+LIBFT	=	$(LIBFT_DIRECTORY)libft.a
+LIBFT_DIRECTORY = ./libft/
+LIBFT_HEADERS = $(LIBFT_DIRECTORY)includes/
+
+OBJECTS	=	$(patsubst %.c, %.o, $(SOURCES_LIST))
+
+.PHONY:	all clean fclean re
+
+all	: 	$(NAME)
+
+$(NAME): $(LIBFT) $(OBJECTS)
+		@$(CC) $(CFLAGS) $(LIBRARIES) $(INCLUDES) $(OBJECTS) -o $(NAME)
+
+%.o : %.c $(HEADERS)
+	$(CC) $(CFLAGS) -c $(INCLUDES) $< -o $@
+
+$(LIBFT):
+		@$(MAKE) -sC $(LIBFT_DIRECTORY)
+
+clean:
+		@$(MAKE) clean -sC $(LIBFT_DIRECTORY)
+		@rm -f $(OBJECTS)
+
+fclean:	clean
+		@rm -f $(NAME)
+		@rm -f $(LIBFT)
+
+re:		fclean all
---
+++ includes/push_swap.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acollin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 20:41:28 by acollin           #+#    #+#             */
+/*   Updated: 2021/10/06 20:42:12 by acollin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include <stdint.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <readline.h>
+# include <history.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <sys/wait.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <term.h>
+# include <curses.h>
+# include <signal.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+# include <limits.h>
+
+typedef struct s_data	t_data;
+
+struct		s_data
+{
+
+};
+
+#endif
