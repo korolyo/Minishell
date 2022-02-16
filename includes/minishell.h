6945c14318414541fb62fa8e8ecedfba37b36867
Atom Collin <acollin@mi-s3.msk.21-school.ru>
2022-02-16 13:02:42 +0300

    Some minor fixes

--- includes/minishell.h
+++ includes/minishell.h
@@ -108,7 +108,7 @@ typedef struct	s_var //для хранения переменных окруже
 // Preparsing
 char	*preparse(char *prompt);
 int		unmatched_quotes(char *prompt, int i);
-void	preparse_quotes(char *prompt, int *i);
+void	preparse_quotes(char const *prompt, int *i);
 char	*preparse_delim(char *prompt, int i);
 int		preparse_redir(char *prompt, int i);
 int		preparse_pipe(char *prompt, int i);
